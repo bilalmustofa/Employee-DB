09b33353a4d9f9fd66e504773481ea03e4c84530
bilal <bilalmustofa146@gmail.com>
2026-03-29 02:46:30 -0600

    add some style

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 let addBtn = document.getElementById("addBtn");
 let formPopup = document.getElementById("formPopup");
 let closeBtn = document.getElementById("closeBtn");
+let saveBtn = document.getElementById("saveBtn");
+
+// stores the employee list
+let employees = [];
+
+// LOAD DATA
+fetch("data.json")
+  .then(res => res.json())
+  .then(data => {
+    employees = data;
+    renderEmployees();
+  });
 
 // open to Add Employee Form
 addBtn.onclick = () => {
@@ -11,4 +23,25 @@ addBtn.onclick = () => {
 // close to Add Employee Form
 closeBtn.onclick = () => {
     formPopup.classList.add("hidden");
+}
+
+// ADD EMPLOYEE
+saveBtn.onclick = () => {
+    let newEmp = {
+        id: Date.now(),
+        firstName: document.getElementById("firstName").Value,
+        lastName: document.getElementById("lastName").Value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        salary: document.getElementById("salary").value,
+        dob: document.getElementById("dob").value
+    }
+    employees.push(newEmp);
+    formPopup.classList.add("hidden");
+}
+
+// DISPLAY EMPLOYEES
+function renderEmployees() {
+    let list = document.getElementById("employeeList");
+    list.innerHTML = "";
 }
