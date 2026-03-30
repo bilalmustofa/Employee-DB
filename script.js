d63e88f8355a5a9167bbe9552351261e02ca194d
bilal <bilalmustofa146@gmail.com>
2026-03-30 12:37:29 -0600

    add js clearForm

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Selecting Element 
+// Selecting Element
 let addBtn = document.getElementById("addBtn");
 let formPopup = document.getElementById("formPopup");
 let closeBtn = document.getElementById("closeBtn");
@@ -9,64 +9,78 @@ let employees = [];
 
 // LOAD DATA
 fetch("data.json")
-  .then(res => res.json())
-  .then(data => {
+  .then((res) => res.json())
+  .then((data) => {
     employees = data;
     renderEmployees();
   });
 
 // open to Add Employee Form
 addBtn.onclick = () => {
-    formPopup.classList.remove("hidden");
-}
+  formPopup.classList.remove("hidden");
+};
 
 // close to Add Employee Form
 closeBtn.onclick = () => {
-    formPopup.classList.add("hidden");
-}
+  formPopup.classList.add("hidden");
+};
 
 // ADD EMPLOYEE
 saveBtn.onclick = () => {
-    let newEmp = {
-        id: Date.now(),
-        firstName: document.getElementById("firstName").value,
-        lastName: document.getElementById("lastName").value,
-        email: document.getElementById("email").value,
-        phone: document.getElementById("phone").value,
-        salary: document.getElementById("salary").value,
-        dob: document.getElementById("dob").value
-    }
-    employees.push(newEmp);
-    renderEmployees();
-    formPopup.classList.add("hidden");
-}
+  let firstName = document.getElementById("firstName").value.trim();
+  let lastName = document.getElementById("lastName").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let phone = document.getElementById("phone").value.trim();
+  let salary = document.getElementById("salary").value.trim();
+  let dob = document.getElementById("dob").value.trim();
+
+  let newEmp = {
+    id: Date.now(),
+    firstName,
+    lastName,
+    email,
+    phone,
+    salary,
+    dob,
+  };
+
+  if (!firstName || !lastName || !email || !phone || !salary || !dob) {
+    alert("Please fill all fields!");
+    return;
+  }
+
+  employees.push(newEmp);
+  renderEmployees();
+  formPopup.classList.add("hidden");
+  clearForm();
+};
 
 // DISPLAY EMPLOYEES
 function renderEmployees() {
-    let list = document.getElementById("employeeList");
-    list.innerHTML = "";
+  let list = document.getElementById("employeeList");
+  list.innerHTML = "";
 
-    employees.forEach(emp => {
-        let div = document.createElement("div");
-        div.className = "employee";
-        div.innerHTML = `
+  employees.forEach((emp) => {
+    let div = document.createElement("div");
+    div.className = "employee";
+    div.innerHTML = `
         <strong>${emp.firstName} ${emp.lastName}</strong> <br>
         <button onclick ="deleteEmployee(${emp.id})">Delete</button>`;
-        div.onclick =  () => renderSingleEmployee(emp.id);
-        list.appendChild(div);
-    });
+    div.onclick = () => renderSingleEmployee(emp.id);
+    list.appendChild(div);
+  });
 }
 
 // DELETE
 function deleteEmployee(id) {
-  employees = employees.filter(emp => emp.id !== id);
+  employees = employees.filter((emp) => emp.id !== id);
   renderEmployees();
   document.getElementById("detailsContent").innerHTML = "Click an employee";
 }
 
 // SHOW SINGLE EMPLOYEE
 function renderSingleEmployee(id) {
-  let emp = employees.find(e => e.id === id);
+  let emp = employees.find((e) => e.id === id);
   let details = document.getElementById("detailsContent");
 
   details.innerHTML = `
@@ -76,4 +90,14 @@ function renderSingleEmployee(id) {
     <p><b>Salary:</b> ${emp.salary}</p>
     <p><b>DOB:</b> ${emp.dob}</p>
   `;
+}
+
+// Clear Form
+function clearForm() {
+  document.getElementById("firstName").value = "";
+  document.getElementById("lastName").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("salary").value = "";
+  document.getElementById("dob").value = "";
 }
